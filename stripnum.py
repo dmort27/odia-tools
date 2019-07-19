a0cf39cc14a0cdd7ac8c99f2b113b5ca5c70e3bd
David R. Mortensen <dmortens@cs.cmu.edu>
2019-07-19 11:25:54 -0400

    Used right group

--- stripnum.py
+++ stripnum.py
@@ -7,4 +7,4 @@ NUM_WITH_SUFF = regex.compile('(\p{N}*)([^\p{N}]+)')
 def strip(token):
     """If token includes a numeric prefix, omit it and return the suffix."""
     m = NUM_WITH_SUFF.match(token)
-    return m.group(1)
+    return m.group(2)
