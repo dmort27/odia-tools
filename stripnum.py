a07d0073599a808ec2cdc79b6f5f80c3856108a0
David R. Mortensen <dmortens@cs.cmu.edu>
2019-07-19 11:23:28 -0400

    Added stripnum.py

---
+++ stripnum.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import regex
+
+NUM_WITH_SUFF = regex.compile('(\p{N}*)([^\p{N}]+)')
+
+def strip(token):
+    """If token includes a numeric prefix, omit it and return the suffix."""
+    m = NUM_WITH_SUFF.match(token)
+    return m.group(1)
